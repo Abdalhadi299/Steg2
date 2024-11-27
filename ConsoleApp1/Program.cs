e835b7cc2bbfb5cb2344b6fe3ed1eff23d51b97b
Abdalhadi Darkoushe <abdalhadi.darkoushe@elev.ga.ntig.se>
2024-11-27 15:04:38 +0100

    first commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,66 @@
+﻿
+//List<string> toys = new List<string> { "Docka", "Bilbana", "Lego", "Gosedjur", "Boll" };
+
+
+
+
+
+//class Program
+//{
+//  static void Main()
+//{
+//  List<string> leksaker = new List<string> { "Docka", "Bilbana", "Lego", "Gosedjur", "Boll" };
+
+//  for (int i = 0; i < leksaker.Count; i++)
+// {
+//    Console.WriteLine(leksaker[i]);
+//}
+
+
+//foreach (string leksak in leksaker)
+//{
+//  Console.WriteLine(leksak);
+//Console.ReadLine();
+//}  
+
+//}  
+//}
+
+
+
+
+
+//List<string> classmates = ["Hussien", "David", "Bilal", "Alex", "Edvin" ];
+//Console.ReadLine();
+
+
+//List<int> Number = [2, 5, 6,8, 10 ];
+//Console.ReadLine();
+
+
+
+
+
+
+//{
+  //  {
+    //    
+        //string[] classmates = ["Hussien", "David", "Bilal", "Alex", "Edvin"];
+      //  
+        
+       // string[] toys = { "Docka", "Bilbana", "Lego", "Gosedjur", "boll" };
+        
+        
+       // int[] ratings =  [2, 5, 6,8, 10 ];
+
+        // Loopa genom och kombinera data
+       // for (int i = 0; i < classmates.Length; i++)
+        //{
+       //     Console.WriteLine($"{classmates[i]} ger {toys[i]} betyget {ratings[i]}.");
+        //    Console.ReadLine();
+       // }
+  //  }
+//}
+
+List<string> cities = new List<string>();
+
